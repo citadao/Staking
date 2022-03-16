51a4a698cdcd23b304008a754792ce206ec9bb76
Brent Fitzgerald <burnto@gmail.com>
2022-03-16 14:22:52 -0700

    format script

--- Autotask.js
+++ Autotask.js
@@ -1,32 +1,37 @@
 const { ethers } = require("ethers");
-const { DefenderRelaySigner, DefenderRelayProvider } = require('defender-relay-client/lib/ethers');
+const {
+  DefenderRelaySigner,
+  DefenderRelayProvider,
+} = require("defender-relay-client/lib/ethers");
 
-
-const Holder_ABI =[            //RewardsHolder Contract ABI
-	{
-		"inputs": [],
-		"name": "sendRewardtoStakingContract",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	}
+const Holder_ABI = [
+  //RewardsHolder Contract ABI
+  {
+    inputs: [],
+    name: "sendRewardtoStakingContract",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
 ];
 
-
-exports.handler =  function(credentials) {                       //Get the credentials, don't need to touch this!
+exports.handler = function (credentials) {
+  //Get the credentials, don't need to touch this!
   const provider = new DefenderRelayProvider(credentials);
-  const signer = new DefenderRelaySigner(credentials, provider, { speed: 'fast' });
+  const signer = new DefenderRelaySigner(credentials, provider, {
+    speed: "fast",
+  });
   return exports.main(signer);
-}
+};
 
-exports.main = async function(signer) {
-    const ContractAddress='0xC003a84217DD703a503eF3dC33de3A4B649d5975';           //RewardsHolder Contract Address
-  	const instance = new ethers.Contract(ContractAddress, Holder_ABI, signer);
+exports.main = async function (signer) {
+  const ContractAddress = "0xC003a84217DD703a503eF3dC33de3A4B649d5975"; //RewardsHolder Contract Address
+  const instance = new ethers.Contract(ContractAddress, Holder_ABI, signer);
 
- 	try {  const SendTX = await instance.sendRewardtoStakingContract();
-				return SendTX;
-       }
-
-	catch {console.log('not working')
-       }
-}
+  try {
+    const SendTX = await instance.sendRewardtoStakingContract();
+    return SendTX;
+  } catch {
+    console.log("not working");
+  }
+};
