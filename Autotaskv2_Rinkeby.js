e2e7542e3f3279500dc72a8c183004189fd1ae9e
Brent Fitzgerald <burnto@gmail.com>
2022-03-16 14:29:25 -0700

    format scripts

--- Autotaskv2_Rinkeby.js
+++ Autotaskv2_Rinkeby.js
@@ -1,51 +1,59 @@
 const { ethers } = require("ethers");
-const { DefenderRelaySigner, DefenderRelayProvider } = require('defender-relay-client/lib/ethers');
+const {
+  DefenderRelaySigner,
+  DefenderRelayProvider,
+} = require("defender-relay-client/lib/ethers");
 
-
-const ERC20_ABI =[            //ERC20 Contract ABI - Transfer Function
+const ERC20_ABI = [
+  //ERC20 Contract ABI - Transfer Function
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "address",
-        "name": "recipient",
-        "type": "address"
+        internalType: "address",
+        name: "recipient",
+        type: "address",
       },
       {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
     ],
-    "name": "transfer",
-    "outputs": [
+    name: "transfer",
+    outputs: [
       {
-        "internalType": "bool",
-        "name": "",
-        "type": "bool"
-      }
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
     ],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }
+    stateMutability: "nonpayable",
+    type: "function",
+  },
 ];
 
-
-exports.handler =  function(credentials) {                       
+exports.handler = function (credentials) {
   const provider = new DefenderRelayProvider(credentials);
-  const signer = new DefenderRelaySigner(credentials, provider, { speed: 'fast' });
+  const signer = new DefenderRelaySigner(credentials, provider, {
+    speed: "fast",
+  });
   return exports.main(signer);
-}
+};
 
-exports.main = async function(signer) {
-    const KNIGHTContractAddress='0x9ab27A62130D56eFc4F8CF69717Ef3BD56103A90';           //mKNIGHT Contract Address on Rinkeby 
-    const instance = new ethers.Contract(KNIGHTContractAddress, ERC20_ABI, signer);
-    const RewardRate = ethers.utils.parseUnits('100');                                 //100mKNIGHT tokens as the RewardRate
-    const StakingContractAddress = '0xD537D030b871BE2d5d99257bd4781a434639AB2f';       //Staking Contract Address on Rinkeby
- 
-	try {  const SendTX = await instance.transfer(StakingContractAddress,RewardRate);   //Transfer mKNIGHT to the Staking Contract on Rinkeby
-				return SendTX;
-       }
+exports.main = async function (signer) {
+  const KNIGHTContractAddress = "0x9ab27A62130D56eFc4F8CF69717Ef3BD56103A90"; //mKNIGHT Contract Address on Rinkeby
+  const instance = new ethers.Contract(
+    KNIGHTContractAddress,
+    ERC20_ABI,
+    signer
+  );
+  const RewardRate = ethers.utils.parseUnits("100"); //100mKNIGHT tokens as the RewardRate
+  const StakingContractAddress = "0xD537D030b871BE2d5d99257bd4781a434639AB2f"; //Staking Contract Address on Rinkeby
 
-	catch {console.log('not working')
-       }
-}
+  try {
+    const SendTX = await instance.transfer(StakingContractAddress, RewardRate); //Transfer mKNIGHT to the Staking Contract on Rinkeby
+    return SendTX;
+  } catch {
+    console.log("not working");
+  }
+};
